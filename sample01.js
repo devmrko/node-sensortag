c53d09f3adef2b09242923a0ad7cc34401273247
devmrko <devmrko@gmail.com>
2016-02-23 15:34:14 +0900

    revert to working copy

--- sample01.js
+++ sample01.js
@@ -15,93 +15,76 @@ function discoverOne() {
 
 function onDiscover(sensorTag) {
 
-    async.series([
-      function (callback) {
-        if(tags.indexOf(sensorTag.uuid) >= 0) {
-          util.log('existed, sensorTag count is ' + (tags.length));
-          util.log('tags: ' + tags);
-        } else {
-          util.log('new one, sensorTag count is ' + (tags.length));
-          util.log('tags: ' + tags); 
-          tags.push(sensorTag.uuid);
-        }
-        callback();      
-      },
-      function (callback) {
-        console.log('>> start');
-        sensorTag.connectAndSetUp(readDevice);
-        sensorTag.once('disconnect', function () {
-          util.log('>> disconnected!');
-          sensorTag.removeListener('disconnect', function() {
-            util.log('>> connection event is removed');
-          });
-          sensorTag.removeListener('gyroscopeChange', function() {
-            util.log('>> gyroscopeChange event is removed');
-          });
+  console.log('>> start');
+  sensorTag.connectAndSetUp(readDevice);
+  sensorTag.once('disconnect', function () {
+    util.log('>> disconnected!');
+    sensorTag.removeListener('disconnect', function() {
+      util.log('>> connection event is removed');
+    });
+    sensorTag.removeListener('gyroscopeChange', function() {
+      util.log('>> gyroscopeChange event is removed');
+    });
+  });
+  console.log('>> end');
+  
+  function readDevice(error) {
+    if(error != undefined) {
+      console.log('>> error: ', error);
+    } else {
+      console.log('>> readDeviceName');
+      sensorTag.readDeviceName(function (error, deviceName) {
+        console.log('\tdevice name = ' + deviceName);
+        sensorTag.discoverServicesAndCharacteristics(function () {
+          setTimeout(function () {
+            console.log('>> discovered services and characteristics');
+            gyroscopeHandler(sensorTag);
+          }, 1000);
+        }); // discoverServicesAndCharacteristics
+      });
+    }
+  }
+  
+  function gyroscopeHandler(sensorTag) {
+    sensorTag.enableGyroscope(function () {
+      sensorTag.setGyroscopePeriod(500, function () {
+        sensorTag.on('gyroscopeChange', function (x, y, z) {
+          console.log('\tx = %d ', x);
+          console.log('\ty = %d ', y);
+          console.log('\tz = %d ', z);
+          var dataObj = {'x-axis': x, 'y-axis': y, 'z-axis': z};
+          writeDataToMongo(dataObj);
         });
-        callback();
-      },
-      function (cllback) {
-        console.log('>> end');
-        discoverOne();
+        sensorTag.notifyGyroscope(function (err) {
+          console.log('>> notifyGyroscope:', err);
+        });
+      }); // setGyroscopePeriod
+    }); // enableGyroscope
+  }
+  
+  function writeDataToMongo(dataObj) {
+    MongoClient.connect("mongodb://jmbox.myqnapcloud.com:27018/iotDB", function(err, db) {
+      if(err) {
+        return console.dir(err);
       }
-    ]);
-
-function readDevice(error) {
-  if(error != undefined) {
-    console.log('>> error: ', error);
-  } else {
-    console.log('>> readDeviceName');
-    sensorTag.readDeviceName(function (error, deviceName) {
-      console.log('\tdevice name = ' + deviceName);
-      sensorTag.discoverServicesAndCharacteristics(function () {
-        setTimeout(function () {
-          console.log('>> discovered services and characteristics');
-          gyroscopeHandler(sensorTag);
-        }, 1000);
-      }); // discoverServicesAndCharacteristics
+      var collection = db.collection('test');
+      collection.insert(dataObj);
+      console.log(">>> mongoDB inserted");
     });
   }
-}
-
-}
-
-function gyroscopeHandler(sensorTag) {
-  sensorTag.enableGyroscope(function () {
-    sensorTag.setGyroscopePeriod(500, function () {
-      sensorTag.on('gyroscopeChange', function (x, y, z) {
-        console.log('\tx = %d ', x);
-        console.log('\ty = %d ', y);
-        console.log('\tz = %d ', z);
-        var dataObj = {'x-axis': x, 'y-axis': y, 'z-axis': z};
-        writeDataToMongo(dataObj);
-      });
-      sensorTag.notifyGyroscope(function (err) {
-        console.log('>> notifyGyroscope:', err);
-      });
-    }); // setGyroscopePeriod
-  }); // enableGyroscope
-}
-
-function writeDataToMongo(dataObj) {
-  MongoClient.connect("mongodb://jmbox.myqnapcloud.com:27018/iotDB", function(err, db) {
-    if(err) {
-      return console.dir(err);
-    }
-    var collection = db.collection('test');
-    collection.insert(dataObj);
-    console.log(">>> mongoDB inserted");
-  });
-}
-function readSystemId() {
-  console.log('>> readSystemId');
-  sensorTag.readSystemId(function (error, systemId) {
-    console.log('\tsystem id = ' + systemId);
-    setTimeout(function () {
-      readDevice(); // start over again
-    }, 1000);
-  });
+  
+  function readSystemId() {
+    console.log('>> readSystemId');
+    sensorTag.readSystemId(function (error, systemId) {
+      console.log('\tsystem id = ' + systemId);
+      setTimeout(function () {
+        readDevice(); // start over again
+      }, 1000);
+    });
+  }
+  
+  discoverOne();
+  
 }
 
 discoverOne();
-
