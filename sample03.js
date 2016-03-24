c81c574bebb63e19014b4a397914a8eefb61448d
joungmin ko <devmrko@gmail.com>
2016-03-24 15:32:28 +0900

    add mqtt usage, and etc

--- sample03.js
+++ sample03.js
@@ -4,10 +4,16 @@ var SensorTag = require('./index');
 
 var async = require('async');
 var util = require('util');
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
+
+var d = new Date();
+
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://10.23.7.8');
 
 var tags = [];
 var cnt = 1;
+var gSystemId = '';
 
 function discoverOne() {
   SensorTag.discover(onDiscover);
@@ -17,7 +23,8 @@ function onDiscover(sensorTag) {
 
   console.log('>> start');
   sensorTag.connectAndSetUp(readDevice);
-  sensorTag.once('disconnect', function () {
+
+  sensorTag.on('disconnect', function () {
     util.log('>> disconnected!');
     sensorTag.disableGyroscope(function () {
       util.log('>> disable gyroscope');
@@ -38,6 +45,7 @@ function onDiscover(sensorTag) {
       console.log('>> readDeviceName');
       sensorTag.readDeviceName(function (error, deviceName) {
         console.log('\tdevice name = ' + deviceName);
+        gSystemId = deviceName;
         sensorTag.discoverServicesAndCharacteristics(function () {
           setTimeout(function () {
             console.log('>> discovered services and characteristics');
@@ -69,11 +77,27 @@ function onDiscover(sensorTag) {
     sensorTag.enableGyroscope(function () {
       sensorTag.setGyroscopePeriod(2000, function () {
         sensorTag.on('gyroscopeChange', function (x, y, z) {
-          console.log('\tx = %d ', x);
-          console.log('\ty = %d ', y);
-          console.log('\tz = %d ', z);
-          var dataObj = {'x-axis': x, 'y-axis': y, 'z-axis': z, cur_date: new Date()};
-          writeDataToMongo(dataObj);
+          async.parallel([
+		function(callback) {
+	          var dataObj = {x: x, y: y, z: z};
+		  callback(null, dataObj);			
+		},
+                function(callback) {
+	          //sensorTag.readSerialNumber(function(error, serialNumber) {
+        	    //console.log(">> systemId: " + serialNumber);
+                    callback(null, sensorTag.id);
+	          //});
+                }
+          ],
+		// optional callback
+		function(err, results){
+		    // results is now equal to ['one', 'two']
+		  d = new Date();
+		  writeDataToMqtt(results[1] + '/x-axis', results[0].x + ":" + d.toGMTString());
+	          writeDataToMqtt(results[1] + '/y-axis', results[0].y + ":" + d.toGMTString());
+	          writeDataToMqtt(results[1] + '/z-axis', results[0].z + ":" + d.toGMTString());
+		}
+	  );
         });
         sensorTag.notifyGyroscope(function (err) {
           console.log('>> notifyGyroscope:', err);
@@ -93,6 +117,11 @@ function onDiscover(sensorTag) {
     });
   }
   
+  function writeDataToMqtt(topic, dataObj) {
+    client.publish('sensors/' + topic, dataObj);
+    console.log(">>> mqtt published: " + topic + ", value: " + dataObj.toString());
+  }
+
   function readSystemId() {
     console.log('>> readSystemId');
     sensorTag.readSystemId(function (error, systemId) {
